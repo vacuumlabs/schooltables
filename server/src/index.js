5035983afa5511f25d36a2f1f4a33c622169c67f
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-06-08 16:41:58 +0200

    Swap cra-universal server, deployable

--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const app = require('./app')
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  console.log(`CRA Server listening on port ${PORT}!`)
-})
---
+++ server/universal.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const fs = require('fs')
+
+const React = require('react')
+const {renderToString} = require('react-dom/server')
+const {StaticRouter} = require('react-router-dom')
+
+const {default: App} = require('../src/App')
+
+module.exports = function universalLoader(req, res) {
+  const filePath = path.resolve(__dirname, '..', 'build', 'index.html')
+
+  fs.readFile(filePath, 'utf8', (err, htmlData) => {
+    if (err) {
+      console.error('read err', err)
+      return res.status(404).end()
+    }
+    const context = {}
+    const markup = renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    )
+
+    if (context.url) {
+      // Somewhere a `<Redirect>` was rendered
+      res.redirect(301, context.url)
+    } else {
+      // we're good, send the response
+      const RenderedApp = htmlData.replace('{{SSR}}', markup)
+      res.send(RenderedApp)
+    }
+  })
+}
