efc143811b9d97e1dfe24fcd7742e1ad086fa93a
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-06-08 11:59:11 +0200

    Prettier-eslint fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,13 @@
-const path = require('path');
+import {createReactAppExpress, handleUniversalRender} from '@cra-express/core'
+const path = require('path')
 
-const React = require('react');
-import { createReactAppExpress, handleUniversalRender } from '@cra-express/core';
+const React = require('react')
 
-const {default: App} = require('../../src/App');
-const clientBuildPath = path.resolve(__dirname, 'client');
+const {default: App} = require('../../src/App')
+const clientBuildPath = path.resolve(__dirname, 'client')
 const app = createReactAppExpress({
   clientBuildPath,
-  universalRender: handleUniversalRender(<App />)
-});
+  universalRender: handleUniversalRender(<App />),
+})
 
-module.exports = app;
+module.exports = app
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const app = require('./app');
+const app = require('./app')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`CRA Server listening on port ${PORT}!`);
-});
+  console.log(`CRA Server listening on port ${PORT}!`)
+})
