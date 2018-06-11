c711c5c95ccabe628cd354603065ed4cf21a807f
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-06-11 11:05:14 +0200

    Remove universal rendering

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const compression = require('compression')
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
+const fs = require('fs')
 
 require('babel-register')({
   ignore: /\/(build|node_modules)\//,
@@ -11,9 +12,7 @@ require('babel-register')({
 })
 
 // routes
-const index = require('./routes/index')
-const api = require('./routes/api')
-const universalLoader = require('./universal')
+const api = require('./api')
 
 const app = express()
 
@@ -27,14 +26,21 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Setup logger
 app.use(morgan('combined'))
 
-app.use('/', index)
-
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
 app.use('/api', api)
 
-// Always return the main index.html, so react-router render the route in the client
-app.use('/', universalLoader)
+app.use('*', (req, res) => {
+  const filePath = path.resolve(__dirname, '..', 'build', 'index.html')
+
+  fs.readFile(filePath, 'utf8', (err, htmlData) => {
+    if (err) {
+      console.error('read err', err)
+      return res.status(404).end()
+    }
+    res.send(htmlData)
+  })
+})
 
 module.exports = app
