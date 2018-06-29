75cfc3f8b16f6943093d6b0a8175a53ec2e1aac0
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-06-29 17:17:39 +0200

    Fix when no database_url

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,11 @@
 module.exports = {
   client: 'pg',
-  connection: `${process.env.DATABASE_URL}?ssl=true` || {
-    host: 'localhost',
-    user: 'schooltables',
-    password: 'dev',
-    database: 'schooltables',
-  },
+  connection: process.env.DATABASE_URL
+    ? `${process.env.DATABASE_URL}?ssl=true`
+    : {
+      host: 'localhost',
+      user: 'schooltables',
+      password: 'dev',
+      database: 'schooltables',
+    },
 }
